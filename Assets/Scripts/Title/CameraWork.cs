bbea5ef1b82a54a3884bbeeafd6bf23f1f3a2cb4
miyazun <g2054795@gmail.com>
2023-04-14 14:42:11 +0900

    タイトルからじゃんけんのシーンに入るときのアニメーションを入れました

--- Assets/Scripts/Title/CameraWork.cs
+++ Assets/Scripts/Title/CameraWork.cs
@@ -2,28 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CameraWork : MonoBehaviour
 {
     [SerializeField] private SceneChanger scene;
 
     [SerializeField] private float zoomPer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Image fade;
 
     public void ToBattleChange()
     {
         var cam = gameObject.GetComponent<Camera>();
-        gameObject.transform.DOMove(new Vector2(0.0f, -2.0f), 1f).OnUpdate(() => cam.orthographicSize -= Time.deltaTime * zoomPer).OnComplete(() => scene.ChnageScene("PlayScene"));
+        gameObject.transform.DOMove(new Vector2(0.0f, -2.0f), 1.2f)
+            .OnUpdate(() => cam.orthographicSize -= Time.deltaTime * zoomPer)
+            .OnComplete(() => scene.ChnageScene("PlayScene"));
+        fade.DOFade(1.0f, 1.0f).SetEase(Ease.InQuad);
     }
 }
